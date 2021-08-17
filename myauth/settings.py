4d270188c86bdaa76483d80839c71bed732f6a88
rebecca-agyei <rebecca.agyei@amalitech.org>
2021-08-17 14:50:06 +0000

    Initial commit

--- myauth/settings.py
+++ myauth/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/account/login'
 LOGIN_REDIRECT_URL = 'home'
